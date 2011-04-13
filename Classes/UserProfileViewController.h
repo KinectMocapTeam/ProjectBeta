27c30fd7f1617cd3cfa84de50f91aa3bc9f3b3dc
Paige Ponzeka <paige@disrupto.com>
2011-04-12 23:26:08 -0400

    some visual edits to signin screen

--- Classes/UserProfileViewController.h
+++ Classes/UserProfileViewController.h
@@ -9,7 +9,7 @@
 #import <UIKit/UIKit.h>
 
 
-@interface UserProfileViewController : UIViewController {
+@interface UserProfileViewController : UITabBarController{
 
 }
 - (IBAction)yellowButtonPressed;
